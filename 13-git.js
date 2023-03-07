ef0086a9ada3d348582c41ad356028c0b83dc8ce
mcwong644 <mcwong644@gmail.com>
2023-03-07 18:46:23 +0800

    commit march7 c

--- 13-git.js
+++ 13-git.js
@@ -5,6 +5,7 @@
 // git config --global init.default branch main (create branch main)
 // git config -h (for help)
 // git init (initialize repository new folder .git created)
+// create .gitignore (to ignore files)
 // git add . or got add <file>
 // git status (to view status)
 // git remote add origin https://github.com/mcwong644/nodetutorial.git (create)
