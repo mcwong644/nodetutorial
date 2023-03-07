573f6d7e76c60e8504f0c8f491d0408535638689
mcwong644 <mcwong644@gmail.com>
2023-03-07 18:38:02 +0800

    commit march7 c

--- lodash.js
+++ lodash.js
@@ -1,6 +1,9 @@
 // ** HTTP start at 1:34 in https://youtu.be/Oe421EPjeBE **
 // lodash starts 2:03
-
+// in command prompt or terminal
+// npm init -y (initialize and create package.json)
+// npm i lodash [to install package for local, or npm -g lodash (global)]
+// 
 const _ = require('lodash');
 
 const items=[1,[2,[3,4]]];
