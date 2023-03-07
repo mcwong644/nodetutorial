c8bc27a342cb103a1fb954fc944e7daed8a8d3ca
mcwong644 <mcwong644@gmail.com>
2023-03-07 18:14:13 +0800

    commit march7 b

---
+++ 13-git.js
@@ -0,0 +1,11 @@
+// use git bash
+// cd c:\nodetutorial
+// git config --global user.name "mcwong644"
+// git config --global user.email mcwong644@gmail.com
+// git config --global init.default branch main
+// git config -h (for help)
+// git init (initialize repository new folder .git created)
+// git add . or got add <file>
+// git status (to view status)
+// git remote add origin https://github.com/mcwong644/nodetutorial.git
+// git push -u origin main
