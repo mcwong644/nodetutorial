4dbc3adcb0165c938b85439a43e70da8f0b1af9f
mcwong644 <mcwong644@gmail.com>
2023-08-05 19:36:18 +0800

    aug5

--- dg01_server.js
+++ dg01_server.js
@@ -1,10 +1,16 @@
 //https://youtu.be/f2EqECiTBL8
 //Node.js Full Course for Beginners | Complete All-in-One Tutorial | 7 Hours
-// dave gray, stop 
+// dave gray, stop 15:09
 
 
 const path=require("path")
 const os=require('os');
+// const math=require("./dg01_math")
+const{ add,subtract,multiply, divide} = require("./dg01_math")
+
+// console.log(math.add(50,60));
+console.log(`add function called ${add(3,4)}`);
+console.log(`divide function called ${divide(30,4)}`);
 console.log(os.type());
 console.log(os.version());
 console.log(os.homedir());
@@ -17,3 +23,4 @@ console.log("=============");
 console.log(path.dirname(__filename));
 console.log(path.basename(__filename));
 console.log(path.extname(__filename));
+console.log(path.parse(__filename));
